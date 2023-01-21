15942dcda85e20edb46f2d38a7f213774723a4a7
VitaliyYakovenko <111122626+VitaliyYakovenko@users.noreply.github.com>
2023-01-21 14:47:37 +0000

    Deploying to gh-pages from @ VitaliyYakovenko/goit-js-hw-09@ac3b7014a17007fd05b5682d8e4675fa584ab2c5 🚀

---
+++ 01-color-switcher.ec400778.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(d){t.disabled=!0,e.disabled=!1,timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){return t.disabled=!1,e.disabled=!0,void clearInterval(timerId)})),e.disabled=!0;
+//# sourceMappingURL=01-color-switcher.ec400778.js.map
